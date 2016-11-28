ae22de6c6272244127179277a965d1adbb0fd4c9
meyerde2 <d_dennis.meyer@web.de>
2016-11-28 21:43:40 +0100

    edited Application-Class for external Jetty

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,6 +1,5 @@
 package app;
 
-
 import app.dashboard.DashboardController;
 import app.dashboard.DashboardDao;
 import app.dashboard.DashboardDaoImpl;
@@ -14,27 +13,17 @@ import app.user.UserDao;
 import app.user.UserDaoImpl;
 import app.util.Filters;
 import app.util.FreeMarkerEngine;
-import app.util.MessageBundle;
-import app.util.Path;
 import freemarker.cache.ClassTemplateLoader;
 import freemarker.template.Configuration;
 import org.sql2o.Sql2o;
-import spark.ModelAndView;
 import spark.Spark;
 import spark.debug.DebugScreen;
+import spark.servlet.SparkApplication;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import static app.util.RequestUtil.getSessionCurrentUser;
-import static app.util.RequestUtil.getSessionLocale;
 import static spark.Spark.*;
 
 
-/**
- * Created by Dennis on 18.10.2016.
- */
-public class Application {
+public class Application implements SparkApplication{
 
 
     public static UserDao userDao;
@@ -43,190 +32,151 @@ public class Application {
 
     public static  FreeMarkerEngine freeMarkerEngine;
 
+
+    /*
     public static void main(String args[]) {
 
-            Spark.staticFileLocation("/public");
+        Spark.staticFileLocation("/public");
 
-            //Test
-            DebugScreen.enableDebugScreen();
+        //Test
+        DebugScreen.enableDebugScreen();
 
-            freeMarkerEngine = new FreeMarkerEngine();
-            Configuration freeMarkerConfiguration = new Configuration();
+        freeMarkerEngine = new FreeMarkerEngine();
+        Configuration freeMarkerConfiguration = new Configuration();
 
-            freeMarkerConfiguration.setTemplateLoader(new ClassTemplateLoader(Application.class, "/public/templates/"));
-            freeMarkerEngine.setConfiguration(freeMarkerConfiguration);
-
-
-            try {
-                Class.forName("com.mysql.jdbc.Driver");
-                String DB_URL = "jdbc:mysql://localhost:3306/goodIT";
-                String USER = "root";
-                String PASS = "";
-                Sql2o sql2o = new Sql2o(DB_URL, USER, PASS);
-
-                userDao = new UserDaoImpl(sql2o);
-                gameDao = new GameDaoImpl(sql2o);
-                dashboardDao = new DashboardDaoImpl(sql2o);
-
-                System.out.println(userDao.getAllUsers());
-
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            }
+        freeMarkerConfiguration.setTemplateLoader(new ClassTemplateLoader(Application.class, "/public/templates/"));
+        freeMarkerEngine.setConfiguration(freeMarkerConfiguration);
 
 
-            // Set up before-filters (called before each get/post)
-            before("*", Filters.addTrailingSlashes);
-            before("*", Filters.handleLocaleChange);
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String DB_URL = "jdbc:mysql://localhost:3306/goodIT";
+            String USER = "root";
+            String PASS = "";
+            Sql2o sql2o = new Sql2o(DB_URL, USER, PASS);
 
-            //Login
-            get("/login/", LoginController.serveLoginPage);
-            post("/login/", LoginController.handleLoginPost);
+            userDao = new UserDaoImpl(sql2o);
+            gameDao = new GameDaoImpl(sql2o);
+            dashboardDao = new DashboardDaoImpl(sql2o);
 
-            //Logout
-            post("/logout/", LoginController.handleLogoutPost);
+            System.out.println(userDao.getAllUsers());
 
-            //Index
-            get("/index/", IndexController.serveIndexPage);
-
-            //Dashboard
-            get("/dashboard/", DashboardController.serveDashboardPage);
-
-            //Game
-            get("/game/", GameController.serveGamePage);
-            get("/gamecontrol/", GameController.serveGamecontrolPage);
-            get("/question/:id/", GameController.serveOneQuestion);
-
-            post("/createNewQuestion/", GameController.createNewQuestion);
-            post("/updateQuestion/", GameController.updateQuestion);
-            post("/gamescore/", GameController.evaluateGameScore);
-            post("/restartGame/", GameController.restartGame);
-
-            //User
-            get("/usercontrol/", UserController.serveUsercontrolPage);
-
-            get("/user/:username/", UserController.serveOneUser);
-
-            post("/createNewUser/", UserController.serveNewUser);
-            post("/updateUser/", UserController.updateUser);
-
-            //Set up after-filters (called after each get/post)
-            after("*", Filters.addGzipHeader);
-
-
-            get("/hello", (request, response) -> {
-
-                System.out.println("hello world......");
-
-                Map<String, Object> attributes = new HashMap<>();
-                attributes.put("message", "Hello World, my Friend!");
-
-                attributes.put("users", userDao.getAllUsers());
-
-                // The login.ftl file is located in directory:
-                // src/test/resources/spark/template/freemarker
-
-                attributes.put("msg", new MessageBundle(getSessionLocale(request)));
-                attributes.put("currentUser", getSessionCurrentUser(request));
-
-                attributes.put("WebPath", Path.Web.class);
-
-                return new ModelAndView(attributes, "/login.ftl");
-            }, new FreeMarkerEngine());
-
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         }
 
-/*
+
+        // Set up before-filters (called before each get/post)
+        before("*", Filters.addTrailingSlashes);
+        before("*", Filters.handleLocaleChange);
+
+        //Login
+        get("/login/", LoginController.serveLoginPage);
+        post("/login/", LoginController.handleLoginPost);
+
+        //Logout
+        post("/logout/", LoginController.handleLogoutPost);
+
+        //Index
+        get("/index/", IndexController.serveIndexPage);
+
+        //Dashboard
+        get("/dashboard/", DashboardController.serveDashboardPage);
+
+        //Game
+        get("/game/", GameController.serveGamePage);
+        get("/gamecontrol/", GameController.serveGamecontrolPage);
+        get("/question/:id/", GameController.serveOneQuestion);
+
+        post("/createNewQuestion/", GameController.createNewQuestion);
+        post("/updateQuestion/", GameController.updateQuestion);
+        post("/gamescore/", GameController.evaluateGameScore);
+        post("/restartGame/", GameController.restartGame);
+
+        //User
+        get("/usercontrol/", UserController.serveUsercontrolPage);
+
+        get("/user/:username/", UserController.serveOneUser);
+
+        post("/createNewUser/", UserController.serveNewUser);
+        post("/updateUser/", UserController.updateUser);
+
+        //Set up after-filters (called after each get/post)
+        after("*", Filters.addGzipHeader);
+
+    }
+*/
 
     @Override
     public void init() {
+        Spark.staticFileLocation("/public");
 
-            Spark.staticFileLocation("/public");
-            DebugScreen.enableDebugScreen();
+        //Test
+        DebugScreen.enableDebugScreen();
 
-            freeMarkerEngine = new FreeMarkerEngine();
-            Configuration freeMarkerConfiguration = new Configuration();
+        freeMarkerEngine = new FreeMarkerEngine();
+        Configuration freeMarkerConfiguration = new Configuration();
 
-            freeMarkerConfiguration.setTemplateLoader(new ClassTemplateLoader(Application.class, "/public/templates/"));
-            freeMarkerEngine.setConfiguration(freeMarkerConfiguration);
+        freeMarkerConfiguration.setTemplateLoader(new ClassTemplateLoader(Application.class, "/public/templates/"));
+        freeMarkerEngine.setConfiguration(freeMarkerConfiguration);
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String DB_URL = "jdbc:mysql://localhost:3306/goodIT";
+            String USER = "root";
+            String PASS = "";
+            Sql2o sql2o = new Sql2o(DB_URL, USER, PASS);
+
+            userDao = new UserDaoImpl(sql2o);
+            gameDao = new GameDaoImpl(sql2o);
+            dashboardDao = new DashboardDaoImpl(sql2o);
+
+            System.out.println(userDao.getAllUsers());
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
 
 
-            try {
-                Class.forName("com.mysql.jdbc.Driver");
-                String DB_URL = "jdbc:mysql://localhost:3306/goodIT";
-                String USER = "root";
-                String PASS = "";
-                Sql2o sql2o = new Sql2o(DB_URL, USER, PASS);
+        // Set up before-filters (called before each get/post)
+        before("*", Filters.addTrailingSlashes);
+        before("*", Filters.handleLocaleChange);
 
-                userDao = new UserDaoImpl(sql2o);
-                gameDao = new GameDaoImpl(sql2o);
+        //Login
+        get("/login/", LoginController.serveLoginPage);
+        post("/login/", LoginController.handleLoginPost);
 
-                System.out.println(userDao.getAllUsers());
+        //Logout
+        post("/logout/", LoginController.handleLogoutPost);
 
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            }
+        //Index
+        get("/index/", IndexController.serveIndexPage);
 
-            // Set up before-filters (called before each get/post)
-            before("*", Filters.addTrailingSlashes);
-            before("*", Filters.handleLocaleChange);
+        //Dashboard
+        get("/dashboard/", DashboardController.serveDashboardPage);
 
-            //Login
-            get("/login/", LoginController.serveLoginPage);
-            post("/login/", LoginController.handleLoginPost);
+        //Game
+        get("/game/", GameController.serveGamePage);
+        get("/gamecontrol/", GameController.serveGamecontrolPage);
+        get("/question/:id/", GameController.serveOneQuestion);
 
-            //Logout
-            post("/logout/", LoginController.handleLogoutPost);
+        post("/createNewQuestion/", GameController.createNewQuestion);
+        post("/updateQuestion/", GameController.updateQuestion);
+        post("/gamescore/", GameController.evaluateGameScore);
+        post("/restartGame/", GameController.restartGame);
 
-            //Index
-            get("/index/", IndexController.serveIndexPage);
+        //User
+        get("/usercontrol/", UserController.serveUsercontrolPage);
 
-            //Dashboard
-            get("/dashboard/", DashboardController.serveDashboardPage);
+        get("/user/:username/", UserController.serveOneUser);
 
-            //Game
-            get("/game/", GameController.serveGamePage);
-            get("/gamecontrol/", GameController.serveGamecontrolPage);
-            get("/question/:id/", GameController.serveOneQuestion);
+        post("/createNewUser/", UserController.serveNewUser);
+        post("/updateUser/", UserController.updateUser);
 
-            post("/createNewQuestion/", GameController.createNewQuestion);
-            post("/updateQuestion/", GameController.updateQuestion);
-            post("/gamescore/", GameController.evaluateGameScore);
-            post("/restartGame/", GameController.restartGame);
-
-            //User
-            get("/usercontrol/", UserController.serveUsercontrolPage);
-
-            get("/user/:username/", UserController.serveOneUser);
-
-            post("/createNewUser/", UserController.serveNewUser);
-            post("/updateUser/", UserController.updateUser);
-
-            //Set up after-filters (called after each get/post)
-            after("*", Filters.addGzipHeader);
-
-        get("/hello", (request, response) -> {
-
-                System.out.println("hello world......");
-
-                Map<String, Object> attributes = new HashMap<>();
-                attributes.put("message", "Hello World, my Friend!");
-
-                attributes.put("users", userDao.getAllUsers());
-
-                // The login.ftl file is located in directory:
-                // src/test/resources/spark/template/freemarker
-
-                attributes.put("msg", new MessageBundle(getSessionLocale(request)));
-                attributes.put("currentUser", getSessionCurrentUser(request));
-
-                attributes.put("WebPath", Path.Web.class);
-
-                return new ModelAndView(attributes, "/login.ftl");
-            }, new FreeMarkerEngine());
+        //Set up after-filters (called after each get/post)
+        after("*", Filters.addGzipHeader);
 
     }
 
-*/
 
 }
